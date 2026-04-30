852da0b3820caa0470a84341f857c551a6e86179
Aryan-Vishwakarma007 <aryanvishwakarma1707@gmail.com>
2026-04-30 19:20:25 +0530

    added main

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,85 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+class Main {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("NO COMMAND GIVEN");
+            return;
+        }
+        String command = args[0];
+        if (command.equals("add")) {
+            if (args.length < 2) {
+                System.out.println("provide a task");
+                return;
+            }
+            String task = args[1];
+            saveTask(task);
+        } else if (command.equals("show")) {
+            showTasks();
+        } else if (command.equals("update")) {
+            if (args.length < 3) {
+                System.out.println("provide a task");
+                return;
+            }
+            String tobereplce = args[1];
+            String isreplacing = args[2];
+            updatetasks(tobereplce, isreplacing);
+
+        }
+
+    }
+
+    static void saveTask(String task) {
+        try {
+            FileWriter fw = new FileWriter("tasks.json", true); // true = append
+            fw.write(task + "\n");
+            fw.close();
+        } catch (Exception e) {
+            System.out.println("Error saving task");
+        }
+    }
+
+    static void showTasks() {
+        String filePath = "C:\\CODING\\Java_Project\\Task_Tracker\\Tracker\\src\\tasks.json";
+        try (BufferedReader reader = new BufferedReader(new FileReader("tasks.json"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.out.println("error ");
+        }
+    }
+
+    static void updatetasks(String tobereplce, String isreplacing) {
+        ArrayList<String> lines = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader("tasks.json"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                if (line.equals(tobereplce)){
+                    lines.add(isreplacing);          //matlab bc yaha ye hora h ki arraylist me saare line aare h but tobereplace ke jagha par isreplacing aara h
+                }
+                else {
+                    lines.add(line);
+                }
+            }
+        }
+        catch (IOException e){
+            System.out.println("error in update");
+        }
+        try(FileWriter wr = new FileWriter("tasks.json")){
+            for(String st : lines){
+                wr.write(st + "\n");
+            }
+        }catch (IOException e){
+            System.out.println("Error in re writing");
+        }
+
+    }
+
 }
